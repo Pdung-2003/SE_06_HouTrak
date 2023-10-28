f22110aa4017cbaeca9365b684ae65a57086bbdc
dungpd2003 <dung.pd215265@sis.hust.edu.vn>
2023-10-28 16:49:08 +0700

    first commit

---
+++ src/view/Colors.java
@@ -0,0 +1,13 @@
+package view;
+
+import java.awt.*;
+
+public class Colors {
+    public static final Color nen_Chung = new Color(204, 255, 255);
+    public static final Color vien_Tren_Duoi = new Color(3, 4, 94);
+    public static final Color  khung_Chung = new Color(144,224,239);
+
+    public static final Color  button_Chung = new Color(0	,119,	182);
+    public static final Color  menu_Trai = new Color(2,	62	,138);
+
+}
