c6bc062f05dc99d06899a9e65e84b158bcef6eb0
Vumanh-mvp <vuducmanh10a@gmail.com>
2023-11-21 10:57:14 +0700

    Manh 21/11

--- src/test/DatabaseConnector.java
+++ src/test/DatabaseConnector.java
@@ -3,20 +3,23 @@ package test;
 import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DatabaseConnector {
     private static SQLServerDataSource ds;
+    static {
+        ds = new SQLServerDataSource();
+        ds.setUser("sa");
+        ds.setPassword("manhvu123");
+        ds.setServerName("MANHVU");
+        ds.setPortNumber(1433);
+        ds.setDatabaseName("HouTrak");
+        ds.setTrustServerCertificate(true);
+    }
 
     // Kiểm tra đăng nhập
     public static boolean checkLogin(String username, String password) {
-        // Khởi tạo kết nối đến cơ sở dữ liệu
-            ds = new SQLServerDataSource();
-            ds.setUser("sa");
-            ds.setPassword("2308");
-            ds.setServerName("DESKTOP-2MP8OGB");
-            ds.setPortNumber(1433);
-            ds.setDatabaseName("HouTrak");
-            ds.setTrustServerCertificate(true);
         try (Connection conn = ds.getConnection()) {
             String query = "SELECT * FROM users WHERE username = ? AND password = ?";
 
@@ -34,4 +37,46 @@ public class DatabaseConnector {
 
         return false;
     }
+
+    // 1. Hien thi danh sach cac ho khau
+    public static List<HoKhau> getDanhSachHoKhau() {
+        List<HoKhau> danhSachHoKhau = new ArrayList<>();
+
+        try (Connection conn = ds.getConnection()) {
+            String query = "SELECT * FROM HoKhau";
+
+            try (PreparedStatement pstmt = conn.prepareStatement(query)) {
+                try (ResultSet rs = pstmt.executeQuery()) {
+                    while (rs.next()) {
+                        int stt = rs.getInt("STT");
+                        String maHoKhau = rs.getString("MaHoKhau");
+                        String hoTenChuHo = rs.getString("HoTenChuHo");
+                        Date ngayLap = rs.getDate("NgayLap");
+                        String diaChi = rs.getString("DiaChi");
+                        String khuVuc = rs.getString("KhuVuc");
+
+                        HoKhau hoKhau = new HoKhau(stt, maHoKhau, hoTenChuHo, ngayLap, diaChi, khuVuc);
+                        danhSachHoKhau.add(hoKhau);
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return danhSachHoKhau;
+    }
+
+
+    // 2. Them Ho khau
+
+    // 3. Thay doi ho khau
+
+    // 4. Tach ho khau
+
+    // 5. Search
+
+    // 6. Remove
+
+    // 7. Xem lich su
 }
