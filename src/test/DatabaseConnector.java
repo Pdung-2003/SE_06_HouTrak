afc6fc609a346e0aaa68bb3c0d805d91d3174292
GIGABYTE <nghia.tt213581@sis.hust.edu.vn>
2023-11-21 17:17:01 +0700

    Kết nối Database - Nghĩa - 17:16PM

--- src/test/DatabaseConnector.java
+++ src/test/DatabaseConnector.java
@@ -7,9 +7,8 @@ import java.sql.*;
 public class DatabaseConnector {
     private static SQLServerDataSource ds;
 
-    // Kiểm tra đăng nhập
-    public static boolean checkLogin(String username, String password) {
-        // Khởi tạo kết nối đến cơ sở dữ liệu
+    public static Connection getConnection() {
+        if (ds == null) {
             ds = new SQLServerDataSource();
             ds.setUser("sa");
             ds.setPassword("2308");
@@ -17,21 +16,14 @@ public class DatabaseConnector {
             ds.setPortNumber(1433);
             ds.setDatabaseName("HouTrak");
             ds.setTrustServerCertificate(true);
-        try (Connection conn = ds.getConnection()) {
-            String query = "SELECT * FROM users WHERE username = ? AND password = ?";
+        }
 
-            try (PreparedStatement pstmt = conn.prepareStatement(query)) {
-                pstmt.setString(1, username);
-                pstmt.setString(2, password);
-
-                try (ResultSet rs = pstmt.executeQuery()) {
-                    return rs.next(); // Trả về true nếu có kết quả, ngược lại trả về false
-                }
-            }
+        try {
+            return ds.getConnection();
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
-        return false;
+        return null;
     }
 }
