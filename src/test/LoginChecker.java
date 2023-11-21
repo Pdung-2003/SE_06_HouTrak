9643905e266b2c0b521500e27893af765aee8aed
GIGABYTE <nghia.tt213581@sis.hust.edu.vn>
2023-11-21 17:18:33 +0700

    Kiểm tra đăng nhập - Nghĩa - 17:16PM

---
+++ src/test/LoginChecker.java
@@ -0,0 +1,29 @@
+package test;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class LoginChecker {
+    public static boolean checkLogin(String username, String password) {
+        try (Connection conn = DatabaseConnector.getConnection()) {
+            if (conn != null) {
+                String query = "SELECT * FROM users WHERE username = ? AND password = ?";
+
+                try (PreparedStatement pstmt = conn.prepareStatement(query)) {
+                    pstmt.setString(1, username);
+                    pstmt.setString(2, password);
+
+                    try (ResultSet rs = pstmt.executeQuery()) {
+                        return rs.next();
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+}
