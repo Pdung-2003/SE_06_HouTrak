35941627087c8d95c434d3fb6afd61dccb25dedd
Vumanh-mvp <vuducmanh10a@gmail.com>
2023-11-24 22:05:28 +0700

    Manh fixed bugs 24/11 22:05

--- src/test/DatabaseConnector.java
+++ src/test/DatabaseConnector.java
@@ -8,18 +8,16 @@ import java.util.List;
 
 public class DatabaseConnector {
     private static SQLServerDataSource ds;
-
+    static {
+        ds = new SQLServerDataSource();
+        ds.setUser("sa");
+        ds.setPassword("manhvu123");
+        ds.setServerName("MANHVU");
+        ds.setPortNumber(1433);
+        ds.setDatabaseName("HouTrak");
+        ds.setTrustServerCertificate(true);
+    }
     public static Connection getConnection() {
-        if (ds == null) {
-            ds = new SQLServerDataSource();
-            ds.setUser("sa");
-            ds.setPassword("2308");
-            ds.setServerName("DESKTOP-2MP8OGB");
-            ds.setPortNumber(1433);
-            ds.setDatabaseName("HouTrak");
-            ds.setTrustServerCertificate(true);
-        }
-
         try {
             return ds.getConnection();
         } catch (SQLException e) {
