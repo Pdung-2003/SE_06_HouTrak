72fe00d389ab4073e5663d7a4af3fdf62c3c308c
lephuchung <lephuchung003@gmail.com>
2023-12-28 01:01:14 +0700

    update XoaHoKhau 01h01 28/12

--- src/view/hokhau/XoaHoKhau.java
+++ src/view/hokhau/XoaHoKhau.java
@@ -55,16 +55,16 @@ public class XoaHoKhau extends JPanel {
 		this.mainFrame = mainFrame;
 		setBackground(Colors.nen_Chung);
 		setPreferredSize(new Dimension(1581, 994));
-		setLayout(new CardLayout(10, 10));
+		setLayout(new BorderLayout(0, 0));
 
 		JPanel panel_XoaHoKhau = new JPanel();
-		add(panel_XoaHoKhau, "name_4499233121000");
+		add(panel_XoaHoKhau);
 		panel_XoaHoKhau.setLayout(new BorderLayout(0, 0));
 
 		JPanel panel_KhungNoiDungXHK = new JPanel();
 		panel_KhungNoiDungXHK.setPreferredSize(new Dimension(1463, 10));
-		panel_KhungNoiDungXHK.setBorder(new LineBorder(Colors.khung_Noi_Dung, 5, true));
-		panel_KhungNoiDungXHK.setBackground(Colors.khung_Noi_Dung);
+		panel_KhungNoiDungXHK.setBorder(new LineBorder(Colors.khung_Chung, 20, true));
+		panel_KhungNoiDungXHK.setBackground(Colors.khung_Chung);
 		panel_XoaHoKhau.add(panel_KhungNoiDungXHK, BorderLayout.CENTER);
 		panel_KhungNoiDungXHK.setLayout(new BorderLayout(0, 0));
 
@@ -82,49 +82,36 @@ public class XoaHoKhau extends JPanel {
 		JPanel panel_XHK_KhoangTrang3 = new JPanel();
 		panel_XHK_01.add(panel_XHK_KhoangTrang3, BorderLayout.SOUTH);
 
-		JPanel panel_XHK_KhoangTrang4 = new JPanel();
-		panel_XHK_01.add(panel_XHK_KhoangTrang4, BorderLayout.EAST);
-
 		panel_XHK_KhoangTrang1.setBackground(Colors.khung_Chung);
 		panel_XHK_KhoangTrang1.setLayout(new BorderLayout(0, 0));
 
 		JLabel lbl_XHK_01_Title = new JLabel("   Nhập mã hộ khẩu: ");
 		lbl_XHK_01_Title.setFont(new Font("Arial", Font.PLAIN, 16));
 		panel_XHK_KhoangTrang1.add(lbl_XHK_01_Title, BorderLayout.CENTER);
-
-		JPanel panel_XHK_01_dem = new JPanel();
-		panel_XHK_01_dem.setBackground(Colors.input_Colors);
-		panel_XHK_KhoangTrang1.add(panel_XHK_01_dem, BorderLayout.EAST);
 		panel_XHK_KhoangTrang2.setBackground(Colors.khung_Noi_Dung);
 		panel_XHK_KhoangTrang3.setBackground(Colors.khung_Noi_Dung);
-		panel_XHK_KhoangTrang4.setBackground(Colors.input_Colors);
 
 		JPanel panel_XHK_01_content = new JPanel();
 		panel_XHK_01.add(panel_XHK_01_content, BorderLayout.CENTER);
-		panel_XHK_01_content.setLayout(new BoxLayout(panel_XHK_01_content, BoxLayout.X_AXIS));
 		panel_XHK_01_content.setBackground(Colors.input_Colors);
+		panel_XHK_01_content.setLayout(new BorderLayout(0, 0));
 
 		txt_XHK_TImKiem = new JTextField();
 		txt_XHK_TImKiem.setBorder(null);
 		panel_XHK_01_content.add(txt_XHK_TImKiem);
 		txt_XHK_TImKiem.setHorizontalAlignment(SwingConstants.LEFT);
-		txt_XHK_TImKiem.setFont(new Font("Arial", Font.PLAIN, 14));
-		txt_XHK_TImKiem.setMargin(new Insets(2, 20, 2, 2));
-		txt_XHK_TImKiem.setCaretColor(Colors.chu_Mo);
-		txt_XHK_TImKiem.setForeground(new Color(0, 0, 0));
+		txt_XHK_TImKiem.setFont(new Font("Arial", Font.PLAIN, 16));
 		txt_XHK_TImKiem.setCursor(Cursor.getPredefinedCursor(Cursor.TEXT_CURSOR));
-		txt_XHK_TImKiem.setPreferredSize(new Dimension(1389, 48));
-		txt_XHK_TImKiem.setBackground(new Color(217, 217, 217));
-		txt_XHK_TImKiem.setColumns(10);
 
 		btn_XHK_01_TimKiem = new JButton("Tìm kiếm");
+		btn_XHK_01_TimKiem.setFont(new Font("Arial", Font.PLAIN, 16));
 		btn_XHK_01_TimKiem.setMargin(new Insets(10, 16, 10, 16));
 		btn_XHK_01_TimKiem.setBackground(Colors.button_Chung);
 		btn_XHK_01_TimKiem.setForeground(Color.WHITE);
 		btn_XHK_01_TimKiem.setOpaque(true);
 		btn_XHK_01_TimKiem.setBorderPainted(false);
 		btn_XHK_01_TimKiem.setMargin(new Insets(10, 16, 10, 16));
-		panel_XHK_01_content.add(btn_XHK_01_TimKiem);
+		panel_XHK_01_content.add(btn_XHK_01_TimKiem, BorderLayout.EAST);
 
 		JPanel panel_XHK_02 = new JPanel();
 		panel_XHK_02.setBorder(null);
@@ -147,31 +134,31 @@ public class XoaHoKhau extends JPanel {
 		panel_XHK_CotTrai_01.add(lbl_XHK_CotTrai_1);
 		lbl_XHK_CotTrai_1.setMaximumSize(new Dimension(100, 14));
 		lbl_XHK_CotTrai_1.setPreferredSize(new Dimension(23, 50));
-		lbl_XHK_CotTrai_1.setFont(new Font("Arial", Font.PLAIN, 12));
+		lbl_XHK_CotTrai_1.setFont(new Font("Arial", Font.PLAIN, 16));
 
 		JLabel lbl_XHK_CotTrai_2 = new JLabel("     Khu vực");
 		panel_XHK_CotTrai_01.add(lbl_XHK_CotTrai_2);
 		lbl_XHK_CotTrai_2.setPreferredSize(new Dimension(23, 500));
 		lbl_XHK_CotTrai_2.setMaximumSize(new Dimension(100, 14));
-		lbl_XHK_CotTrai_2.setFont(new Font("Arial", Font.PLAIN, 12));
+		lbl_XHK_CotTrai_2.setFont(new Font("Arial", Font.PLAIN, 16));
 
 		JLabel lbl_XHK_CotTrai_3 = new JLabel("     Địa chỉ");
 		panel_XHK_CotTrai_01.add(lbl_XHK_CotTrai_3);
 		lbl_XHK_CotTrai_3.setPreferredSize(new Dimension(23, 500));
 		lbl_XHK_CotTrai_3.setMaximumSize(new Dimension(100, 14));
-		lbl_XHK_CotTrai_3.setFont(new Font("Arial", Font.PLAIN, 12));
+		lbl_XHK_CotTrai_3.setFont(new Font("Arial", Font.PLAIN, 16));
 
 		JLabel lbl_XHK_CotTrai_5 = new JLabel("     Chủ hộ");
 		panel_XHK_CotTrai_01.add(lbl_XHK_CotTrai_5);
 		lbl_XHK_CotTrai_5.setPreferredSize(new Dimension(23, 500));
 		lbl_XHK_CotTrai_5.setMaximumSize(new Dimension(100, 14));
-		lbl_XHK_CotTrai_5.setFont(new Font("Arial", Font.PLAIN, 12));
+		lbl_XHK_CotTrai_5.setFont(new Font("Arial", Font.PLAIN, 16));
 
 		JLabel lbl_XHK_CotTrai_4 = new JLabel("     Ngày lập");
 		panel_XHK_CotTrai_01.add(lbl_XHK_CotTrai_4);
 		lbl_XHK_CotTrai_4.setPreferredSize(new Dimension(23, 500));
 		lbl_XHK_CotTrai_4.setMaximumSize(new Dimension(100, 14));
-		lbl_XHK_CotTrai_4.setFont(new Font("Arial", Font.PLAIN, 12));
+		lbl_XHK_CotTrai_4.setFont(new Font("Arial", Font.PLAIN, 16));
 
 		JPanel panel_XHK_CotTrai_02 = new JPanel();
 		panel_XHK_CotTrai_02.setBackground(Colors.khung_Noi_Dung);
@@ -183,7 +170,7 @@ public class XoaHoKhau extends JPanel {
 		lbl_XHK_CotTrai_6.setHorizontalAlignment(SwingConstants.LEFT);
 		lbl_XHK_CotTrai_6.setMaximumSize(new Dimension(200, 14));
 		lbl_XHK_CotTrai_6.setMinimumSize(new Dimension(50, 14));
-		lbl_XHK_CotTrai_6.setFont(new Font("Arial", Font.PLAIN, 12));
+		lbl_XHK_CotTrai_6.setFont(new Font("Arial", Font.PLAIN, 16));
 
 		JPanel panel_XHK_CotPhai = new JPanel();
 		panel_XHK_CotPhai.setBackground(Colors.khung_Chung);
@@ -196,20 +183,20 @@ public class XoaHoKhau extends JPanel {
 		panel_XHK_CotPhai.add(panel_XNK_CotPhai_01);
 		panel_XNK_CotPhai_01.setLayout(new GridLayout(5, 1, 0, 0));
 
-		lbl_XNK_CotPhai_MaHK.setFont(new Font("Arial", Font.PLAIN, 12));
+		lbl_XNK_CotPhai_MaHK.setFont(new Font("Arial", Font.PLAIN, 16));
 		panel_XNK_CotPhai_01.add(lbl_XNK_CotPhai_MaHK);
 
-		lbl_XNK_CotPhai_KhuVuc.setFont(new Font("Arial", Font.PLAIN, 12));
+		lbl_XNK_CotPhai_KhuVuc.setFont(new Font("Arial", Font.PLAIN, 16));
 		panel_XNK_CotPhai_01.add(lbl_XNK_CotPhai_KhuVuc);
 
-		lbl_XNK_CotPhai_DiaChi.setFont(new Font("Arial", Font.PLAIN, 12));
+		lbl_XNK_CotPhai_DiaChi.setFont(new Font("Arial", Font.PLAIN, 16));
 		panel_XNK_CotPhai_01.add(lbl_XNK_CotPhai_DiaChi);
 
 		// panel nay de bang thong tin chu ho
-		lbl_XNK_CotPhai_ChuHo.setFont(new Font("Arial", Font.PLAIN, 12));
+		lbl_XNK_CotPhai_ChuHo.setFont(new Font("Arial", Font.PLAIN, 16));
 		panel_XNK_CotPhai_01.add(lbl_XNK_CotPhai_ChuHo);
 
-		lbl_XNK_CotPhai_NgayLap.setFont(new Font("Arial", Font.PLAIN, 12));
+		lbl_XNK_CotPhai_NgayLap.setFont(new Font("Arial", Font.PLAIN, 16));
 		panel_XNK_CotPhai_01.add(lbl_XNK_CotPhai_NgayLap);
 
 		//dien bang thong tin thanh vien thuoc ho khau vao day
