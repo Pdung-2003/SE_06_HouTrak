4fe4497d964c4a1e222fe47f5eb1cc681518fe52
GIGABYTE <nghia.tt213581@sis.hust.edu.vn>
2023-12-27 23:45:00 +0700

    Thống kê phát thưởng tạm thời - 11:43PM 27/12/2023

--- src/controller/phat_thuong/ThongKePhatThuongController.java
+++ src/controller/phat_thuong/ThongKePhatThuongController.java
@@ -1,4 +1,94 @@
 package controller.phat_thuong;
 
+import model.DatabaseConnector;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ThongKePhatThuongController {
+    public List<Object[]> timPhatThuongTheoThoiGian(
+            String ngayBatDau, String thangBatDau, String namBatDau,
+            String ngayKetThuc, String thangKetThuc, String namKetThuc) {
+
+        List<Object[]> ketQuaThongKe = new ArrayList<>();
+        Connection conn = DatabaseConnector.getConnection();
+
+        if (conn != null) {
+            String query = "SELECT PT.MaNhanKhau, NK.HoTen, PT.Lop, NK.NgaySinh, PT.MaPhanThuong, PT.SoLuong, PT.SoTien " +
+                    "FROM ChinhSachThuong PT " +
+                    "JOIN NhanKhau NK ON PT.MaNhanKhau = NK.MaNhanKhau " +
+                    "WHERE PT.Date BETWEEN ? AND ?";
+            try (PreparedStatement pstmt = conn.prepareStatement(query)) {
+                String ngayThangNamBatDau = namBatDau + "-" + thangBatDau + "-" + ngayBatDau;
+                String ngayThangNamKetThuc = namKetThuc + "-" + thangKetThuc + "-" + ngayKetThuc;
+                pstmt.setString(1, ngayThangNamBatDau);
+                pstmt.setString(2, ngayThangNamKetThuc);
+
+                ResultSet rs = pstmt.executeQuery();
+                while (rs.next()) {
+                    Object[] row = new Object[]{
+                            rs.getString("MaNhanKhau"),
+                            rs.getString("TenNhanKhau"),
+                            rs.getInt("Lop"),
+                            rs.getDate("NgaySinh"),
+                            rs.getString("PhanThuong"),
+                            rs.getInt("SoLuong"),
+                            rs.getFloat("SoTien")
+                    };
+                    ketQuaThongKe.add(row);
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    conn.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return ketQuaThongKe;
+    }
+
+    public List<Object[]> thongKeTheoThoiGian(
+            String ngayBatDau, String thangBatDau, String namBatDau,
+            String ngayKetThuc, String thangKetThuc, String namKetThuc) {
+
+        List<Object[]> ketQuaThongKe = new ArrayList<>();
+        Connection conn = DatabaseConnector.getConnection();
+
+        if (conn != null) {
+            String query = "SELECT * FROM ThongKe WHERE ThoiGian BETWEEN ? AND ?";
+            try (PreparedStatement pstmt = conn.prepareStatement(query)) {
+                String ngayThangNamBatDau = namBatDau + "-" + thangBatDau + "-" + ngayBatDau;
+                String ngayThangNamKetThuc = namKetThuc + "-" + thangKetThuc + "-" + ngayKetThuc;
+                pstmt.setString(1, ngayThangNamBatDau);
+                pstmt.setString(2, ngayThangNamKetThuc);
+
+                ResultSet rs = pstmt.executeQuery();
+                while (rs.next()) {
+                    Object[] row = new Object[]{
+                            rs.getString("MaNhanKhau"),
+                            rs.getString("DiaChi"),
+                            rs.getInt("SoLanNhan"),
+                            rs.getFloat("SoTienNhanDuoc")
+                    };
+                    ketQuaThongKe.add(row);
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    conn.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return ketQuaThongKe;
+    }
 }
--- src/view/phat_thuong/ThongKePhatThuong.java
+++ src/view/phat_thuong/ThongKePhatThuong.java
@@ -1,27 +1,32 @@
 package view.phat_thuong;
 
 
+import controller.phat_thuong.ThongKePhatThuongController;
 import view.dangnhap.ManHinhChinh;
 import view.settings.Colors;
+import view.settings.CustomRowHeightRenderer;
 
-import javax.swing.JComboBox;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+import javax.swing.*;
 import javax.swing.border.LineBorder;
-
-import java.awt.BorderLayout;
-import java.awt.FlowLayout;
-import java.awt.Font;
-import java.awt.Dimension;
-import java.awt.GridLayout;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+import javax.swing.table.TableRowSorter;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Calendar;
-
-import javax.swing.JButton;
-import javax.swing.BoxLayout;
-import javax.swing.DefaultComboBoxModel;
+import java.util.List;
 
 public class ThongKePhatThuong extends JPanel {
+	private ThongKePhatThuongController controller = new ThongKePhatThuongController();
+	private DefaultTableModel phatThuongTableModel;
+	private JTable phatThuongTable;
+	private TableRowSorter<TableModel> phatThuongSorter;
 
+	private DefaultTableModel thongKeTableModel;
+	private JTable thongKeTable;
+	private TableRowSorter<TableModel> thongKeSorter;
 	/**
 	 * Create the panel.
 	 */
@@ -153,13 +158,44 @@ public class ThongKePhatThuong extends JPanel {
 		panel_TKPT_Filter_Content_EndTime.add(lblTong_In_Ra);
 
 
-
-
 		JPanel panel_TKPT_NoiDung = new JPanel();// Điền bảng kết quả thống kê
 		panel_TKPT_NoiDung.setBackground(Colors.khung_Chung);
 		panel_KhungNoiDungTKPT.add(panel_TKPT_NoiDung, BorderLayout.CENTER);
+		panel_TKPT_NoiDung.setLayout(new GridLayout(2, 1)); // Chia panel thành 2 hàng 1 cột
 
+		// Panel chứa bảng kết quả thống kê PhatThuong và bảng kết quả thống kê
+		JPanel panelTables = new JPanel(new GridLayout(2, 1));
 
+		// Điền bảng kết quả thống kê PhatThuong
+		JPanel panel_TKTT_Content_TablePhatThuong = new JPanel();
+		panel_TKTT_Content_TablePhatThuong.setBackground(Colors.khung_Chung);
+		panel_TKTT_Content_TablePhatThuong.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10));
+		initializeTablePhatThuong(panel_TKTT_Content_TablePhatThuong);
+		panelTables.add(panel_TKTT_Content_TablePhatThuong);
+
+		// Điền bảng kết quả thống kê
+		JPanel panel_TKTT_Content_TableThongKe = new JPanel();
+		panel_TKTT_Content_TableThongKe.setBackground(Colors.khung_Chung);
+		panel_TKTT_Content_TableThongKe.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10));
+		initializeTableThongKe(panel_TKTT_Content_TableThongKe);
+		panelTables.add(panel_TKTT_Content_TableThongKe);
+
+		panel_TKPT_NoiDung.add(panelTables);
+		btn_TKPT_Filter_Confirm.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String ngayBatDau = comboBox_TKPT_Filter_Content_StartTime_Ngay.getSelectedItem().toString();
+				String thangBatDau = comboBox_TKPT_Filter_Content_StartTime_Thang.getSelectedItem().toString();
+				String namBatDau = comboBox_TKPT_Filter_Content_StartTime_Nam.getSelectedItem().toString();
+				String ngayKetThuc = comboBox_TKPT_Filter_Content_EndTime_Ngay.getSelectedItem().toString();
+				String thangKetThuc = comboBox_TKPT_Filter_Content_EndTime_Thang.getSelectedItem().toString();
+				String namKetThuc = comboBox_TKPT_Filter_Content_EndTime_Nam.getSelectedItem().toString();
+				List<Object[]> ketQuaPhatThuong = controller.timPhatThuongTheoThoiGian(ngayBatDau, thangBatDau, namBatDau, ngayKetThuc, thangKetThuc, namKetThuc);
+				addDataToTablePhatThuong(ketQuaPhatThuong);
+//				List<String> ketQuaThongKe = controller.thongKeTheoThoiGian(ngayBatDau, thangBatDau, namBatDau, ngayKetThuc, thangKetThuc, namKetThuc);
+//				addDataToTableThongKe(ketQuaThongKe);
+			}
+		});
 	}
 	private void populateYears(JComboBox comboBox) {
 		int currentYear = Calendar.getInstance().get(Calendar.YEAR);
@@ -189,5 +225,171 @@ public class ThongKePhatThuong extends JPanel {
 		Calendar calendar = Calendar.getInstance();
 		calendar.set(year, month - 1, 1);
 		return calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
+	}
+
+	private void initializeTablePhatThuong(JPanel jPanel) {
+		// Khởi tạo hoặc xóa dữ liệu của bảng cũ
+		if (phatThuongTableModel == null) {
+			// Nếu tableModel chưa được khởi tạo, thực hiện khởi tạo mới
+			phatThuongTableModel = new DefaultTableModel();
+			phatThuongTableModel.addColumn("Mã Nhân Khẩu");
+			phatThuongTableModel.addColumn("Họ Tên");
+			phatThuongTableModel.addColumn("Lớp");
+			phatThuongTableModel.addColumn("Ngày Sinh");
+			phatThuongTableModel.addColumn("Phần Thưởng");
+			phatThuongTableModel.addColumn("Số Lượng");
+			phatThuongTableModel.addColumn("Số Tiền");
+		} else {
+			// Nếu tableModel đã tồn tại, xóa tất cả các hàng
+			phatThuongTableModel.setRowCount(0);
+		}
+
+		// Tạo JTable với tableModel
+		phatThuongTable = new JTable(phatThuongTableModel);
+		phatThuongSorter = new TableRowSorter<>(phatThuongTableModel);
+		phatThuongTable.setRowSorter(phatThuongSorter);
+
+		// Cài đặt renderer cho header của bảng
+		phatThuongTable.getTableHeader().setDefaultRenderer(new DefaultTableCellRenderer() {
+			@Override
+			public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+				JLabel label = (JLabel) super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+				label.setFont(label.getFont().deriveFont(Font.BOLD));
+				label.setBackground(Colors.mau_Header);
+				label.setForeground(Colors.mau_Text_QLHK);
+				return label;
+			}
+		});
+
+		// Cài đặt kích thước của các cột
+		int[] columnWidths = {150, 150, 150, 150, 150, 150, 150};
+		for (int i = 0; i < columnWidths.length; i++) {
+			phatThuongTable.getColumnModel().getColumn(i).setPreferredWidth(columnWidths[i]);
+		}
+
+		// Cài đặt renderer mặc định cho tất cả các kiểu dữ liệu
+		int rowHeight = 30;
+		phatThuongTable.setDefaultRenderer(Object.class, new CustomRowHeightRenderer(rowHeight));
+
+		// Cài đặt layout cho jPanel và thêm bảng vào panel
+		jPanel.removeAll(); // Xóa các thành phần cũ trong jPanel trước khi thêm mới
+		jPanel.setLayout(new BorderLayout(10, 10));
+
+		JScrollPane scrollPane = new JScrollPane(phatThuongTable);
+		scrollPane.setPreferredSize(new Dimension(1400, 100)); // Đặt kích thước của JScrollPane
+
+		phatThuongTable.setBackground(Colors.mau_Nen_QLHK);
+		phatThuongTable.setForeground(Colors.mau_Text_QLHK);
+		scrollPane.setBackground(Colors.khung_Chung);
+		jPanel.add(scrollPane, BorderLayout.CENTER);
+
+		JViewport viewport = scrollPane.getViewport();
+		viewport.setBackground(Colors.khung_Chung);
+		scrollPane.setBorder(BorderFactory.createLineBorder(Colors.khung_Chung));
+	}
+
+	private void addDataToTablePhatThuong(List<Object[]> data) {
+		// Xóa dữ liệu hiện tại trong bảng
+		phatThuongTableModel.setRowCount(0);
+
+		// Thêm dữ liệu mới vào bảng từ danh sách List<Object[]>
+		for (Object[] rowData : data) {
+			phatThuongTableModel.addRow(rowData);
+		}
+	}
+
+	private void initializeTableThongKe(JPanel jPanel) {
+		// Khởi tạo hoặc xóa dữ liệu của bảng cũ
+		if (thongKeTableModel == null) {
+			// Nếu tableModel chưa được khởi tạo, thực hiện khởi tạo mới
+			thongKeTableModel = new DefaultTableModel();
+			thongKeTableModel.addColumn("Mã Nhân Khẩu");
+			thongKeTableModel.addColumn("Địa Chỉ");
+			thongKeTableModel.addColumn("Số Lần Nhận");
+			thongKeTableModel.addColumn("Số Tiền Nhận Được");
+		} else {
+			// Nếu tableModel đã tồn tại, xóa tất cả các hàng
+			thongKeTableModel.setRowCount(0);
+		}
+
+		// Tạo JTable với tableModel
+		thongKeTable = new JTable(thongKeTableModel);
+		thongKeSorter = new TableRowSorter<>(thongKeTableModel);
+		thongKeTable.setRowSorter(thongKeSorter);
+
+		// Cài đặt renderer cho header của bảng
+		thongKeTable.getTableHeader().setDefaultRenderer(new DefaultTableCellRenderer() {
+			@Override
+			public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+				JLabel label = (JLabel) super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+				label.setFont(label.getFont().deriveFont(Font.BOLD));
+				label.setBackground(Colors.mau_Header);
+				label.setForeground(Colors.mau_Text_QLHK);
+				return label;
+			}
+		});
+
+		// Cài đặt kích thước của các cột
+		int[] columnWidths = {150, 150, 150, 150};
+		for (int i = 0; i < columnWidths.length; i++) {
+			thongKeTable.getColumnModel().getColumn(i).setPreferredWidth(columnWidths[i]);
+		}
+
+		// Cài đặt renderer mặc định cho tất cả các kiểu dữ liệu
+		int rowHeight = 30;
+		thongKeTable.setDefaultRenderer(Object.class, new CustomRowHeightRenderer(rowHeight));
+
+		// Cài đặt layout cho jPanel và thêm bảng vào panel
+		jPanel.removeAll(); // Xóa các thành phần cũ trong jPanel trước khi thêm mới
+		jPanel.setLayout(new BorderLayout(10, 10));
+
+		JScrollPane scrollPane = new JScrollPane(thongKeTable);
+		scrollPane.setPreferredSize(new Dimension(1400, 100)); // Đặt kích thước của JScrollPane
+
+		thongKeTable.setBackground(Colors.mau_Nen_QLHK);
+		thongKeTable.setForeground(Colors.mau_Text_QLHK);
+		scrollPane.setBackground(Colors.khung_Chung);
+		jPanel.add(scrollPane, BorderLayout.CENTER);
+
+		JViewport viewport = scrollPane.getViewport();
+		viewport.setBackground(Colors.khung_Chung);
+		scrollPane.setBorder(BorderFactory.createLineBorder(Colors.khung_Chung));
+	}
+	private void addDataToTableThongKe(List<String> thongKeList) {
+		// Xóa dữ liệu hiện tại trong bảng
+		thongKeTableModel.setRowCount(0);
+
+		for (String thongKeItem : thongKeList) {
+			// Kiểm tra xem chuỗi có đúng định dạng "Tháng/Năm - Số lượng" hay không
+			if (thongKeItem.contains(" - ")) {
+				String[] parts = thongKeItem.split(" - ");
+				if (parts.length >= 2) { // Kiểm tra xem đã tách được đúng định dạng chưa
+					String thangNam = parts[0]; // Tháng/Năm
+					String soLuong = parts[1]; // Số lượng
+
+					// Tạo các giá trị mặc định cho các cột không có dữ liệu tương ứng
+					String maNhanKhau = ""; // Mã Nhân Khẩu
+					String diaChi = ""; // Địa Chỉ
+					String soLanNhan = ""; // Số Lần Nhận
+					String soTienNhanDuoc = ""; // Số Tiền Nhận Được
+
+					// Thêm dòng mới vào bảng với dữ liệu từ đối tượng String
+					thongKeTableModel.addRow(new Object[]{
+							maNhanKhau,
+							diaChi,
+							soLanNhan,
+							soTienNhanDuoc,
+							thangNam,
+							soLuong
+					});
+				}
+			} else {
+				// Nếu không đúng định dạng, có thể xử lý bằng cách bỏ qua hoặc thông báo lỗi tùy ý
+				// Ví dụ:
+				System.out.println("Dữ liệu không đúng định dạng: " + thongKeItem);
+				// Hoặc có thể bỏ qua và tiếp tục với các dòng dữ liệu tiếp theo:
+				continue;
+			}
+		}
 	}
 }
