2348aceeb380389809546cf838a87799ef7d93cb
Vumanh-mvp <vuducmanh10a@gmail.com>
2024-01-03 18:05:31 +0700

    Updated

--- src/main/TestAll.java
+++ src/main/TestAll.java
@@ -1,12 +1,11 @@
 package main;
 
-import view.dangnhap.ManHinhChinh;
+import view.dangnhap.SignIn;
 
 public class TestAll {
     public static void main(String[] args) {
         try {
-            //new SignIn();
-            new ManHinhChinh();
+            new SignIn();
         } catch (Exception e) {
             e.printStackTrace();
         }
